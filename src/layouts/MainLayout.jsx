4d4f27e8e63d72b88f9df622388af273939c983a
billal990 <billalbelal621@gmail.com>
2024-08-02 19:55:29 +0600

    made small changes to push into main

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -10,7 +10,7 @@ export default function MainLayout() {
     <div>
       <Header/>
       <div className='my-[2rem] text-center h-[5rem]'>
-        <h1>Content goes here .....</h1>
+        <h1>Content goes here ...</h1>
       </div>
       {pathname === "/login" || pathname === "/signup" ? "" : <Footer/>}
     </div>
