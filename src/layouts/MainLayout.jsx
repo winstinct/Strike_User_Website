11760e6f1360ae1f77b9a672d0037d9a8a656e00
billal990 <billalbelal621@gmail.com>
2024-08-03 10:17:11 +0600

    made changes in layout page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,12 +5,11 @@ import { Outlet, useLocation } from 'react-router-dom'
 
 export default function MainLayout() {
   const {pathname} = useLocation();
-  console.log('Current Location===> ', pathname)
   return (
     <div>
       <Header/>
-      <div className='my-[2rem] text-center h-[5rem]'>
-        <h1>Content goes here ...</h1>
+      <div>
+        <Outlet/>
       </div>
       {pathname === "/login" || pathname === "/signup" ? "" : <Footer/>}
     </div>
