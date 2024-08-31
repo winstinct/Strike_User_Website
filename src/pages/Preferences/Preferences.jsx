a9f215b1752395e148bd542e6a3ca4e32c74a67f
billal990 <billalbelal621@gmail.com>
2024-08-31 08:57:54 +0600

    adjusted font size and icon in the faq section

--- src/pages/Preferences/Preferences.jsx
+++ src/pages/Preferences/Preferences.jsx
@@ -2,29 +2,32 @@ import { setActiveStyle } from "../../utils/setActiveStyle";
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function Preferences() {
-  window.scrollTo({top:0, behavior:"smooth"})
+  window.scrollTo({ top: 0, behavior: "smooth" });
   return (
     <main>
       <h3 className="text-[2rem] font-bold italic">Preferences</h3>
-    <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 my-[1.5rem]">
-      <NavLink
-        style={setActiveStyle}
-        className="border-[1px] border-gray-300 rounded-3xl py-3 hover:text-[#5500C3] duration-300 text-[1.25rem] font-bold italic hover:border-[#5500C3] block text-center"
-        to=""
-        end
+      <div
+        style={{ boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.10)" }}
+        className="grid md:grid-cols-2 grid-cols-1 my-[1.5rem] gap-5 rounded-[20px]"
       >
-        Notification Settings
-      </NavLink>
-      <NavLink
-        style={setActiveStyle}
-        className="border-[1px] border-gray-300 rounded-3xl py-3 hover:text-[#5500C3] duration-300 text-[1.25rem] font-bold italic hover:border-[#5500C3] block text-center"
-        to="privacy-settings"
-        end
-      >
-        Privacy Settings
-      </NavLink>
-    </div>
-    <Outlet/>
+        <NavLink
+          style={setActiveStyle}
+          className="block font-bold text-[1.25rem] italic py-3 rounded-[20px] w-full text-center"
+          to=""
+          end
+        >
+          Notification Settings
+        </NavLink>
+        <NavLink
+          style={setActiveStyle}
+          className="block font-bold text-[1.25rem] italic py-3 rounded-[20px] w-full text-center"
+          to="privacy-settings"
+          end
+        >
+          Privacy Settings
+        </NavLink>
+      </div>
+      <Outlet />
     </main>
   );
 }
